684c7f3b1cb5a9eda0b6d5af3eeac4fb59f8ba7d
ArqamPisyst <arqam.ansari@pisystindia.com>
2024-11-15 18:05:28 +0530

    updated

--- lib/Providers/registration_provider.dart
+++ lib/Providers/registration_provider.dart
@@ -1,122 +1,106 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart';
+import '../Models/RequestModels/registration_otp_request_model.dart';
+import '../Models/RequestModels/registration_otp_verification_request_model.dart';
+import '../Models/RequestModels/user_registration_request_model.dart';
+import '../Models/ResponseModels/get_all_country_response_model.dart';
+import '../Models/ResponseModels/registration_otp_response_model.dart';
+import '../Models/ResponseModels/registration_otp_verification_response_model.dart';
+import '../Models/ResponseModels/user_registration_response_model.dart';
+import '../constants.dart';
 
-final String baseUrl = "http://192.168.1.22/dating_backend_springboot/admin";
-
-// Countries
-class RegistrationProvider extends GetConnect{
-    Future<bool> fetchCountries() async {
+class RegistrationProvider extends GetConnect {
+  Future<CountryResponse?> fetchCountries() async {
     try {
-      final response = await get(Uri.parse('$baseUrl/Common/country'));
-      final jsonResponse = jsonDecode(response.body);
+      Response response = await get('$baseUrl/Common/country');
+
       if (response.statusCode == 200) {
-        if(jsonResponse['error']['code']==0){
-          debugPrint('Country: ${jsonResponse['payload']['data']}');
-          return true;
-        }
-        else{
-          debugPrint("Error occured in Countries: ${jsonResponse['error']['message']} Status: ${jsonResponse['error']['code']}");
-          return false;
+        if (response.body['error']['code'] == 0) {
+          return CountryResponse.fromJson(response.body);
+        } else {
+          failure('Error', response.body['error']['message']);
+          return null;
         }
       } else {
-        debugPrint('Failed to load Countries. Status code: ${response.statusCode}');
-       debugPrint("${jsonResponse['error']['message']}");
-        return false;
+        failure('Error', response.body['error']['message']);
+        return null;
       }
     } catch (e) {
-      debugPrint('An error occurred in Countries: $e');
-      return false;
+      failure('Error', e.toString());
+      return null;
     }
   }
 
   // Verify Email using OTP
-  Future<bool> getOtp(dynamic data) async {
+  Future<RegistrationOtpResponse?> sendOtp(
+      RegistrationOTPRequest registrationOTPRequest) async {
     try {
-      final response = await post(
-          Uri.parse('$baseUrl/Authentication/sendotp'),
-          body: jsonEncode(data));
-      final jsonResponse = jsonDecode(response.body);
+      Response response =
+          await post('$baseUrl/Authentication/sendotp', registrationOTPRequest);
+
       if (response.statusCode == 200) {
-        if (jsonResponse['error']['code'] == 0) {
-          debugPrint('Get OTP for Verify Email: ${jsonResponse['payload']['message']}');
-          return true;
+        if (response.body['error']['code'] == 0) {
+          RegistrationOtpResponse.fromJson(response.body);
         } else {
-          debugPrint(
-              "Error occured in Get OTP for Verify Email: ${jsonResponse['error']['message']} Status: ${jsonResponse['error']['code']}");
-          return false;
+          failure('Error', response.body['error']['message']);
+          return null;
         }
       } else {
-        debugPrint(
-            'Failed to load Get OTP for Verify Email. Status code: ${response.statusCode}');
-        debugPrint("${jsonResponse['error']['message']}");
-        return false;
+        failure('Error', response.body['error']['message']);
+        return null;
       }
     } catch (e) {
-      debugPrint('An error occurred in Get OTP for Verify Email: $e');
-      return false;
+      failure('Error', e.toString());
+      return null;
     }
+    return null;
   }
 
- // Verify password while registration
- Future<bool> otpVerification(dynamic data) async {
+  // Verify password while registration
+  Future<RegistrationOtpVerificationResponse?> otpVerification(
+      RegistrationOtpVerificationRequest
+          registrationOtpVerificationRequest) async {
     try {
-      final response = await post(
-          Uri.parse('$baseUrl/Authentication/verifyotp'),
-          body: jsonEncode(data));
-      if (response.body.isEmpty) {
-        debugPrint('Empty response body.');
-        return false;
-      }
-      final jsonResponse = jsonDecode(response.body);
+      Response response = await post('$baseUrl/Authentication/verifyotp',
+          registrationOtpVerificationRequest);
+
       if (response.statusCode == 200) {
-        if (jsonResponse['error']['code'] == 0) {
-          debugPrint('OTP Verification for registration: ${jsonResponse['payload']['message']}');
-          return true;
+        if (response.body['error']['code'] == 0) {
+          return RegistrationOtpVerificationResponse.fromJson(response.body);
         } else {
-          debugPrint(
-              "Error occured in OTP Verification for registration: ${jsonResponse['error']['message']} Status: ${jsonResponse['error']['code']}");
-          return false;
+          failure('Error', response.body['error']['message']);
+          return null;
         }
       } else {
-        debugPrint(
-            'Failed to load OTP Verification for registration. Status code: ${response.statusCode}');
-        debugPrint("${jsonResponse['error']['message']}");
-        return false;
+        failure('Error', response.body['error']['message']);
+        return null;
       }
     } catch (e) {
-      debugPrint('An error occurred in OTP Verification for registration: $e');
-      return false;
+      failure('Error', e.toString());
+      return null;
     }
   }
 
   // User Registration
-  Future<bool> userLogin(Map<String, dynamic> data) async {
+  Future<UserRegistrationResponse?> register(
+      UserRegistrationRequest userRegistrationRequest) async {
     try {
-      final response =
-          await post(Uri.parse('$baseUrl/Authentication/register'), body: jsonEncode(data));
-      final jsonResponse = jsonDecode(response.body);
+      Response response = await post(
+          '$baseUrl/Authentication/register', userRegistrationRequest);
+
       if (response.statusCode == 200) {
-        if (jsonResponse['error']['code'] == 0) {
-          debugPrint('User Login: ${jsonResponse['payload']}');
-          return true;
+        if (response.body['error']['code'] == 0) {
+          return UserRegistrationResponse.fromJson(response.body);
         } else {
-          debugPrint(
-              "Error occured in User Login: ${jsonResponse['error']['message']} Status: ${jsonResponse['error']['code']}");
-          return false;
+          failure('Error', response.body['error']['message']);
+          return null;
         }
       } else {
-        debugPrint(
-            'Failed to load User Login. Status code: ${response.statusCode}');
-        debugPrint("${jsonResponse['error']['message']}");
-        return false;
+        failure('Error', response.body.toString());
+        return null;
       }
     } catch (e) {
-      debugPrint('An error occurred in User Login: $e');
-      return false;
+      failure('Error', e.toString());
+      return null;
     }
   }
-
 }
