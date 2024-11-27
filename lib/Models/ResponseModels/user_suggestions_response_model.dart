3339c40125f10238918e14f85e168bcef5804a04
ArqamPisyst <147052237+ArqamPisyst@users.noreply.github.com>
2024-11-27 15:25:20 +0530

    Update user_suggestions_response_model.dart

--- lib/Models/ResponseModels/user_suggestions_response_model.dart
+++ lib/Models/ResponseModels/user_suggestions_response_model.dart
@@ -1,18 +1,17 @@
 class UserSuggestionsResponseModel {
   final bool success;
   final Payload? payload;
-  final Error error;
+  final Error? error;
 
   UserSuggestionsResponseModel(
-      {required this.success, this.payload, required this.error});
+      {required this.success, this.payload, this.error});
 
   factory UserSuggestionsResponseModel.fromJson(Map<String, dynamic> json) {
     return UserSuggestionsResponseModel(
-      success: json['success'] as bool,
-      payload: json['payload'] != null && (json['payload'] as Map).isNotEmpty
-          ? Payload.fromJson(json['payload'])
-          : null,
-      error: Error.fromJson(json['error']),
+      success: json['success'],
+      payload:
+          json['payload'] != null ? Payload.fromJson(json['payload']) : null,
+      error: json['error'] != null ? Error.fromJson(json['error']) : null,
     );
   }
 
@@ -20,138 +19,180 @@ class UserSuggestionsResponseModel {
     return {
       'success': success,
       'payload': payload?.toJson(),
-      'error': error.toJson(),
+      'error': error?.toJson(),
     };
   }
 }
 
 class Payload {
-  final String message;
-  final List<User>? locationBase;
-  final List<User>? preferenceBase;
-  final List<User>? desireBase;
+  final List<SuggestedUser> locationBase;
+  final List<SuggestedUser> preferenceBase;
+  final List<SuggestedUser> desireBase;
+  final List<SuggestedUser> languageBase;
+  final List<SuggestedUser> highlightedAccount;
 
-  Payload(
-      {required this.message,
-      this.locationBase,
-      this.preferenceBase,
-      this.desireBase});
+  Payload({
+    required this.locationBase,
+    required this.preferenceBase,
+    required this.desireBase,
+    required this.languageBase,
+    required this.highlightedAccount,
+  });
 
   factory Payload.fromJson(Map<String, dynamic> json) {
     return Payload(
-      message: json['message'] as String,
-      locationBase: (json['location_base'] as List<dynamic>?)
-          ?.map((e) => User.fromJson(e as Map<String, dynamic>))
+      locationBase: (json['location_base'] as List? ?? [])
+          .map((item) => SuggestedUser.fromJson(item))
           .toList(),
-      preferenceBase: (json['preference_base'] as List<dynamic>?)
-          ?.map((e) => User.fromJson(e as Map<String, dynamic>))
+      preferenceBase: (json['preference_base'] as List? ?? [])
+          .map((item) => SuggestedUser.fromJson(item))
           .toList(),
-      desireBase: (json['desire_base'] as List<dynamic>?)
-          ?.map((e) => User.fromJson(e as Map<String, dynamic>))
+      desireBase: (json['desire_base'] as List? ?? [])
+          .map((item) => SuggestedUser.fromJson(item))
+          .toList(),
+      languageBase: (json['language_base'] as List? ?? [])
+          .map((item) => SuggestedUser.fromJson(item))
+          .toList(),
+      highlightedAccount: (json['highlighted_account'] as List? ?? [])
+          .map((item) => SuggestedUser.fromJson(item))
           .toList(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'message': message,
-      'location_base': locationBase?.map((e) => e.toJson()).toList(),
-      'preference_base': preferenceBase?.map((e) => e.toJson()).toList(),
-      'desire_base': desireBase?.map((e) => e.toJson()).toList(),
+      'location_base': locationBase.map((e) => e.toJson()).toList(),
+      'preference_base': preferenceBase.map((e) => e.toJson()).toList(),
+      'desire_base': desireBase.map((e) => e.toJson()).toList(),
+      'language_base': languageBase.map((e) => e.toJson()).toList(),
+      'highlighted_account': highlightedAccount.map((e) => e.toJson()).toList(),
     };
   }
 }
 
-class User {
-  final String id;
-  final String name;
-  final String email;
-  final String mobile;
-  final String city;
-  final String address;
-  final String gender;
-  final String subGender;
-  final String countryId;
-  final String password;
-  final String latitude;
-  final String longitude;
-  final String otp;
-  final String type;
-  final String dob;
-  final String nickname;
-  final String interest;
-  final String bio;
-  final String emailAlerts;
-  final String lookingFor;
-  final String username;
-  final String status;
-  final String created;
-  final String updated;
-  final double? distance;
+class SuggestedUser {
+  final String? id;
+  final String? userId;
+  final String? name;
+  final String? email;
+  final String? mobile;
+  final String? city;
+  final String? address;
+  final String? gender;
+  final String? subGender;
+  final String? countryId;
+  final String? password;
+  final String? latitude;
+  final String? longitude;
+  final String? otp;
+  final String? type;
+  final String? dob;
+  final String? nickname;
+  final String? interest;
+  final String? bio;
+  final String? emailAlerts;
+  final String? lookingFor;
+  final String? username;
+  final String? profileImage;
+  final String? userActiveStatus;
+  final String? statusSetting;
+  final String? accountVerificationStatus;
+  final String? accountHighlightStatus;
+  final String? status;
+  final String? created;
+  final String? updated;
+  final String? genderName;
+  final String? subGenderName;
+  final String? countryName;
+  final List<String> images;
 
-  User({
-    required this.id,
-    required this.name,
-    required this.email,
-    required this.mobile,
-    required this.city,
-    required this.address,
-    required this.gender,
-    required this.subGender,
-    required this.countryId,
-    required this.password,
-    required this.latitude,
-    required this.longitude,
-    required this.otp,
-    required this.type,
-    required this.dob,
-    required this.nickname,
-    required this.interest,
-    required this.bio,
-    required this.emailAlerts,
-    required this.lookingFor,
-    required this.username,
-    required this.status,
-    required this.created,
-    required this.updated,
-    this.distance,
+  SuggestedUser({
+    this.id,
+    this.userId,
+    this.name,
+    this.email,
+    this.mobile,
+    this.city,
+    this.address,
+    this.gender,
+    this.subGender,
+    this.countryId,
+    this.password,
+    this.latitude,
+    this.longitude,
+    this.otp,
+    this.type,
+    this.dob,
+    this.nickname,
+    this.interest,
+    this.bio,
+    this.emailAlerts,
+    this.lookingFor,
+    this.username,
+    this.profileImage,
+    this.userActiveStatus,
+    this.statusSetting,
+    this.accountVerificationStatus,
+    this.accountHighlightStatus,
+    this.status,
+    this.created,
+    this.updated,
+    this.genderName,
+    this.subGenderName,
+    this.countryName,
+    this.images = const [],
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      email: json['email'] as String,
-      mobile: json['mobile'] as String,
-      city: json['city'] as String,
-      address: json['address'] as String,
-      gender: json['gender'] as String,
-      subGender: json['sub_gender'] as String,
-      countryId: json['country_id'] as String,
-      password: json['password'] as String,
-      latitude: json['latitude'] as String,
-      longitude: json['longitude'] as String,
-      otp: json['otp'] as String,
-      type: json['type'] as String,
-      dob: json['dob'] as String,
-      nickname: json['nickname'] as String,
-      interest: json['interest'] as String,
-      bio: json['bio'] as String,
-      emailAlerts: json['email_alerts'] as String,
-      lookingFor: json['looking_for'] as String,
-      username: json['username'] as String,
-      status: json['status'] as String,
-      created: json['created'] as String,
-      updated: json['updated'] as String,
-      distance: json['distance'] != null
-          ? (json['distance'] as num).toDouble()
-          : null,
+  factory SuggestedUser.fromJson(Map<String, dynamic> json) {
+    return SuggestedUser(
+      id: json['id'],
+      userId: json['user_id'],
+      name: json['name'],
+      email: json['email'],
+      mobile: json['mobile'],
+      city: json['city'],
+      address: json['address'],
+      gender: json['gender'],
+      subGender: json['sub_gender'],
+      countryId: json['country_id'],
+      password: json['password'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+      otp: json['otp'],
+      type: json['type'],
+      dob: json['dob'],
+      nickname: json['nickname'],
+      interest: json['interest'],
+      bio: json['bio'],
+      emailAlerts: json['email_alerts'],
+      lookingFor: json['looking_for'],
+      username: json['username'],
+      profileImage: json['profile_image'],
+      userActiveStatus: json['user_active_status'],
+      statusSetting: json['status_setting'],
+      accountVerificationStatus: json['account_verification_status'],
+      accountHighlightStatus: json['account_highlight_status'],
+      status: json['status'],
+      created: json['created'],
+      updated: json['updated'],
+      genderName: json['gender_name'],
+      subGenderName: json['sub_gender_name'] ?? json['sub_gender_nm'],
+      countryName: json['country_name'],
+      images: [
+        json['img1'] as String? ?? '',
+        json['img2'] as String? ?? '',
+        json['img3'] as String? ?? '',
+        json['img4'] as String? ?? '',
+        json['img5'] as String? ?? '',
+        json['img6'] as String? ?? '',
+      ].where((img) => img.isNotEmpty).toList(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
+      'user_id': userId,
       'name': name,
       'email': email,
       'mobile': mobile,
@@ -172,24 +213,32 @@ class User {
       'email_alerts': emailAlerts,
       'looking_for': lookingFor,
       'username': username,
+      'profile_image': profileImage,
+      'user_active_status': userActiveStatus,
+      'status_setting': statusSetting,
+      'account_verification_status': accountVerificationStatus,
+      'account_highlight_status': accountHighlightStatus,
       'status': status,
       'created': created,
       'updated': updated,
-      'distance': distance,
+      'gender_name': genderName,
+      'sub_gender_name': subGenderName,
+      'country_name': countryName,
+      'images': images,
     };
   }
 }
 
 class Error {
   final int code;
-  final String message;
+  final String? message;
 
-  Error({required this.code, required this.message});
+  Error({required this.code, this.message});
 
   factory Error.fromJson(Map<String, dynamic> json) {
     return Error(
-      code: json['code'] as int,
-      message: json['message'] as String,
+      code: json['code'],
+      message: json['message'],
     );
   }
 
