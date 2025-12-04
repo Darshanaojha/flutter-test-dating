8d1a8449b7aa3f0bdf7e5cb7965b02c45991132e
Darshana <darshana.ojha@pisystindia.com>
2025-12-04 20:07:51 +0530

    changed minsdkversion to stable

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
 
     defaultConfig {
         applicationId = "com.cajed.in"
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
